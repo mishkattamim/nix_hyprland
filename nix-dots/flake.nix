5fe1048ffa89315ed8731e772b753db834508f70
Mishkat TaMim <103811069+mishkattamim@users.noreply.github.com>
2025-11-18 13:47:26 +0600

    Update flake.nix

--- nix-dots/flake.nix
+++ nix-dots/flake.nix
@@ -2,11 +2,11 @@
   description = "A simple NixOS flake";
 
   inputs = {
-    # NixOS official package source, using the nixos-25.05 branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
     home-manager = {
-    url = "github:nix-community/home-manager/release-25.05"; # Use the branch matching your nixpkgs (24.05) or the latest
+    url = "github:nix-community/home-manager/release-25.05"; 
     inputs.nixpkgs.follows = "nixpkgs";
   };
   
@@ -22,7 +22,7 @@
         
         {
           # Configure Home Manager for your user
-          home-manager.users.ryuuma = import ./home.nix; 
+          home-manager.users.ryuuma = import ./home.nix;
           home-manager.useUserPackages = true;
           home-manager.useGlobalPkgs = true;
           home-manager.backupFileExtension = "backup";
